64a3c114a875a10d3b1a4781dae4c0abc2153c8a
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-05-11 10:17:43 -0300

    refactor: Accessing content in constructor method for ParagraphRender

--- src/lib/presentation/renders/common/paragraph_render.ts
+++ src/lib/presentation/renders/common/paragraph_render.ts
@@ -1,5 +1,8 @@
 import { NodeElement } from "../../../core/renders/elements/node_element";
-import { ElementRender } from "../../../core/renders/element_render";
+import {
+  ElementRender,
+  NewElementRender,
+} from "../../../core/renders/element_render";
 import { Classes } from "../../../core/utils/classes";
 import { UniversalRender } from "../universals/universal_render";
 
@@ -8,14 +11,13 @@ interface ParagraphRenderParams {
   withoutMarginBottom?: boolean;
 }
 
-export class ParagraphRender extends ElementRender<
-  ParagraphRenderParams,
-  HTMLParagraphElement
-> {
-  build(params: ParagraphRenderParams): HTMLParagraphElement {
-    return UniversalRender.createParagraph({
-      classes: [params.withoutMarginBottom ? Classes.noMarginBottom : ""],
-      children: [new NodeElement(UniversalRender.createText(params.text))],
-    });
+export class ParagraphRender extends NewElementRender<ParagraphRenderParams> {
+  build(params: ParagraphRenderParams): NodeElement {
+    return new NodeElement(
+      UniversalRender.createParagraph({
+        classes: [params.withoutMarginBottom ? Classes.noMarginBottom : ""],
+        children: [new NodeElement(UniversalRender.createText(params.text))],
+      })
+    );
   }
 }
