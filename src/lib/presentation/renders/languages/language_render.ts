bfd45a9d83672ab78f302031027043af57405a5c
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-05-11 10:14:55 -0300

    refactor: Accessing content in constructor method for LanguageRender

--- src/lib/presentation/renders/languages/language_render.ts
+++ src/lib/presentation/renders/languages/language_render.ts
@@ -1,5 +1,8 @@
 import { NodeElement } from "../../../core/renders/elements/node_element";
-import { ElementRender } from "../../../core/renders/element_render";
+import {
+  ElementRender,
+  NewElementRender,
+} from "../../../core/renders/element_render";
 import { UserLanguage } from "../../../domain/entities/user_language";
 import { ColumnRender } from "../common/column_render";
 import { ParagraphRender } from "../common/paragraph_render";
@@ -9,29 +12,28 @@ interface LanguageRenderParams {
   userLanguage: UserLanguage;
 }
 
-export class LanguageRender extends ElementRender<
-  LanguageRenderParams,
-  HTMLDivElement
-> {
-  build(params: LanguageRenderParams): HTMLDivElement {
-    const buildedParagraph = new ParagraphRender({
-      text: params.userLanguage.language,
-      withoutMarginBottom: true,
-    });
-    const buildedLevels = this._buildedLevels(params.userLanguage.level);
-    const builded = new ColumnRender({
+export class LanguageRender extends NewElementRender<LanguageRenderParams> {
+  build(params: LanguageRenderParams): NodeElement {
+    return new ColumnRender({
       size: 6,
-      children: [buildedParagraph, ...buildedLevels],
+      children: [
+        new ParagraphRender({
+          text: params.userLanguage.language,
+          withoutMarginBottom: true,
+        }),
+        ...this._buildedLevels(params),
+      ],
     });
-
-    return builded.content;
   }
 
-  private _buildedLevels(levelOfUser: number): NodeElement[] {
+  private _buildedLevels(params: LanguageRenderParams): NodeElement[] {
     let levels: NodeElement[] = [];
 
     for (let i = 0; i < 5; i++) {
-      levels[i] = new CircleRender({ level: i, levelOfUser });
+      levels[i] = new CircleRender({
+        level: i,
+        levelOfUser: params.userLanguage.level,
+      });
     }
 
     return levels;
