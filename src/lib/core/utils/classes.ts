05804011efed70446cc7e5f6507c3f3de3b83d8b
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-05-12 23:07:40 -0300

    fix: Missing classes creation

--- src/lib/core/utils/classes.ts
+++ src/lib/core/utils/classes.ts
@@ -5,11 +5,13 @@ export abstract class Classes {
 
   // Spacement
   static marginBottom2: string = "mb-2";
+  static paddingEnd2: string = "pe-2";
   static paddingStart2: string = "ps-2";
   static noMarginBottom: string = "mb-0";
   static noHorizontalMargin: string = "mx-0";
   static noPadding: string = "p-0";
   static noPaddingEnd: string = "pe-0";
+  static noPaddingStart: string = "ps-0";
 
   // Structure
   static alignItemsCenter: string = "align-items-center";
