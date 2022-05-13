67a5679c4472320597ebb2044011e2178cc1df1c
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-05-13 20:21:23 -0300

    Fully removed city and country from main content

--- src/lib/domain/entities/location_details.ts
+++ src/lib/domain/entities/location_details.ts
@@ -1,17 +1,9 @@
 import { Brazil } from "./brazil";
 
 export class LocationDetails {
-  city?: string | null;
   company: string;
-  country?: string;
 
-  constructor(params: {
-    city?: string | null;
-    company: string;
-    country?: Brazil;
-  }) {
-    this.city = params.city;
+  constructor(params: { company: string }) {
     this.company = params.company;
-    this.country = params.country?.locale;
   }
 }
