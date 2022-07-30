295d711838ae284e385cbc33d01d0c9ed235b065
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2022-07-30 12:41:27 -0300

    chore: Change for mode development in webpack for faster reloads

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,21 @@
 const path = require("path");
 
 module.exports = {
-    mode: "production",
-    entry: path.resolve(__dirname, "./src/index.ts"),
-    module: {
-        rules: [
-            {
-                test: /\.ts?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: [".ts", ".js"],
-    },
-    output: {
-        filename: "script.js",
-        path: path.resolve(__dirname, "public", "static", "bundle"),
-    },
+  entry: path.resolve(__dirname, "./src/index.ts"),
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "public", "static", "bundle"),
+  },
 };
