aa863201b40031a29ad5afb835ebeb07ab53585b
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2024-02-26 22:48:03 +0000

    fix: change en_us with pt_br in primeira mesa experience

--- src/lib/locales/experiencesSection/primeiraMesa.ts
+++ src/lib/locales/experiencesSection/primeiraMesa.ts
@@ -6,14 +6,14 @@ export const primeiraMesa = {
   initialDate: new Date(2021, 4),
   responsabilities: {
     [Language.EN_US]: [
-      "Trabalhei em um APP que dá descontos em reservas de restaurantes. Eles estavam insatisfeitos com a antiga fábrica de software e eu fui responsável por finalizar a funcionalidade de delivery",
-      "Desenvolvimento de novas funcionalidades e arrumar bugs na base de código mobile. Trabalhei em funcionalidades que envolviam sistemas de pagamento e de geolocalização",
-      "Responsável por ensinar e orientar o novo time de desenvolvedores",
-    ],
-    [Language.PT_BR]: [
       "I worked with an APP that give discounts in restaurants reservations. They were unsatisfied with their old software house and I was responsible to finish a delivery system feature",
       "Development of new features and fix bugs into their mobile codebase. Worked in features that involves payments and geolocation",
       "Responsible for teaching and orienting the new developers team.",
+    ],
+    [Language.PT_BR]: [
+      "Trabalhei em um APP que dá descontos em reservas de restaurantes. Eles estavam insatisfeitos com a antiga fábrica de software e eu fui responsável por finalizar a funcionalidade de delivery",
+      "Desenvolvimento de novas funcionalidades e arrumar bugs na base de código mobile. Trabalhei em funcionalidades que envolviam sistemas de pagamento e de geolocalização",
+      "Responsável por ensinar e orientar o novo time de desenvolvedores",
     ],
   },
   title: {
