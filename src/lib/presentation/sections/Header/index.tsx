c6188424a5ed5dedb1c64e08581e2b613b84c0b7
Tulio Pinto Neto <tulio.neto@startaideia.com.br>
2024-03-07 20:34:39 +0000

    refactor: change size of header titles

--- src/lib/presentation/sections/Header/index.tsx
+++ src/lib/presentation/sections/Header/index.tsx
@@ -1,13 +1,13 @@
+import styles from "./styles.module.scss";
+import CN from "classnames";
+
 export const Header = () => {
   return (
     <div className="row justify-content-end align-items-center" id="header">
-    <div className="me-5 pe-5 light-gray" id="header-titles">
-      <h2>Tulio Pinto Neto</h2>
-      <h3>Software Engineer</h3>
+      <div className={CN("my-5", "me-5", "pe-5", styles.headerTitles)}>
+        <h2>Tulio Pinto Neto</h2>
+        <h3>Software Engineer</h3>
+      </div>
     </div>
-    <div id="header-img">
-      <img src="./img/header-photo.jpg" />
-    </div>
-  </div>
   );
-}
+};
